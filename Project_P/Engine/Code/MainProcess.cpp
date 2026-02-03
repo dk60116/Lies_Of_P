d207acebb7137a9e915e625554ec783bdfd12ad2
한택근 <60398466+dk60116@users.noreply.github.com>
2026-02-03 20:55:28 +0900

    Render editor viewport without top offset

--- Project_P/Engine/Code/MainProcess.cpp
+++ Project_P/Engine/Code/MainProcess.cpp
@@ -43,7 +43,7 @@ HRESULT CMainProcess::Initialize()
 #ifndef _CLIENT_BUILD
     CEditor::EDITORWINOPTION sOption = CEditor::GetInstance().Get_Options();
 
-    vector2Int offsetMin = vector2Int(0, (_int)sOption.topBarHeight);
+    vector2Int offsetMin = vector2Int(0, 0);
     vector2Int offsetMax = vector2Int(_int(sOption.projectWidth + sOption.hierachyWidth + sOption.inspectorWidth), 0);
 
     CGraphicDevice::GetInstance().Add_SwapChain
