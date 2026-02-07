7867c6a5a8fa5ea292138a65e4e570ce5cc4d5ea
HTK <dk60116@gmail.com>
2026-02-08 03:36:45 +0900

    Feat: Sprint

--- Project_P/Client/Code/PlayerState_Move.cpp
+++ Project_P/Client/Code/PlayerState_Move.cpp
@@ -1,8 +1,29 @@
 #include "cpch.h"
 #include "PlayerState_Move.h"
 
+void CPlayerState_Move::Initialize(CPlayerControllerContext* _ctx)
+{
+    __super::Initialize(_ctx);
+
+
+    {
+        CAnimationClip* startClip = CResources::GetInstance().LoadOnScene<CAnimationClip>(L"Eve_SprintForward_During (Animation Clip)");
+
+        const wstring clipName = startClip->Get_ResourceName();
+        const _uint frameCount = startClip->Get_FrameCount();
+
+        CAnimationClip::ActionTrigger at = { 16, L"ExitAbleTime11" };
+        startClip->Add_ActionTrigger(at);
+        m_pCtx->Animator()->RegisterActionHandler(L"ExitAbleTime11", [this]()
+            {
+                //m_pCtx->Animator()->Stop();
+            });
+    }
+}
+
 void CPlayerState_Move::Enter()
 {
+    __super::Enter();
 }
 
 void CPlayerState_Move::Update()
@@ -15,6 +36,11 @@ void CPlayerState_Move::Update()
 
     m_pCtx->BeginTurnTo(m_pCtx->GetDesiredYawDeg());
 
+    //if (!m_pCtx->IsSprint())
+    //    m_pCtx->SetAnimMoveSpeed(1.f);
+    //else
+    //    m_pCtx->SetAnimMoveSpeed(2.f);
+
     if (!m_pCtx->IsSprint())
         m_pCtx->SetAnimMoveSpeed(1.f);
     else
@@ -25,4 +51,6 @@ void CPlayerState_Move::Update()
 
     if (m_pCtx->Animator()->GetBool(L"isBattle", b) && m_pCtx->Animator()->GetFloat(L"speed", s))
         CDebug::LogError(to_string((_int)b) + ", " + to_string((_int)s));
+
+    //m_pCtx->Animator()->Play();
 }
--- Project_P/Client/Header/PlayerState_Move.h
+++ Project_P/Client/Header/PlayerState_Move.h
@@ -4,6 +4,7 @@
 class CPlayerState_Move final : public CPlayerState
 {
 public:
+    void Initialize(CPlayerControllerContext* _ctx) override;
     void Enter() override;
     void Update() override;
 };
