bfc55be87e50b633e687bb66a4d9226fba31f4c1
한택근 <60398466+dk60116@users.noreply.github.com>
2026-02-09 14:33:43 +0900

    Fix static mesh data conversion

--- Project_P/Engine/Code/Resources.cpp
+++ Project_P/Engine/Code/Resources.cpp
@@ -70,22 +70,12 @@ HRESULT CResources::ConvertFBXToMeshBufferData(const wstring _filePath)
 
 	if (!aiScene || !aiScene->HasMeshes())
 	{
-		CDebug::LogError(L"Failed create skinned buffer - Invalid scene: " + _filePath);
-		return E_FAIL;
-	}
-
-	_bool hasBones = false;
-	for (_uint i = 0; i < aiScene->mNumMeshes; ++i)
-		hasBones |= aiScene->mMeshes[i]->HasBones();
-
-	if (!hasBones)
-	{
-		CDebug::LogError(L"Failed create skinned buffer - No bones: " + _filePath);
+		CDebug::LogError(L"Failed create mesh buffer - Invalid scene: " + _filePath);
 		return E_FAIL;
 	}
 
 	const _bool hasMaterial = aiScene->HasMaterials();
-	using VTX = VertexSkinnedBuffer;
+	using VTX = VertexTexNormalTangentBuffer;
 
 	vector<aiMatrix4x4> meshGlobalMats(aiScene->mNumMeshes, aiMatrix4x4());
 
@@ -101,7 +91,7 @@ HRESULT CResources::ConvertFBXToMeshBufferData(const wstring _filePath)
 		};
 	BuildMeshTransforms(aiScene->mRootNode, aiMatrix4x4());
 
-	vector<CSkinnedMeshBuffer::SkinnedBufferInitiaizeInfo> bufferInfoList;
+	vector<CMeshBuffer::MeshBufferInitiaizeInfo> bufferInfoList;
 
 	for (_uint i = 0; i < aiScene->mNumMeshes; ++i)
 	{
@@ -109,7 +99,7 @@ HRESULT CResources::ConvertFBXToMeshBufferData(const wstring _filePath)
 		const aiMatrix4x4& gMat = meshGlobalMats[i];
 		aiMatrix3x3 gMat3 = aiMatrix3x3(gMat).Inverse().Transpose();
 
-		CSkinnedMeshBuffer::SkinnedBufferInitiaizeInfo info{};
+		CMeshBuffer::MeshBufferInitiaizeInfo info{};
 		info.meshName = CMeshBuffer::FindMeshName(aiScene, i);
 
 		vector<VTX>   vertices;
@@ -133,33 +123,6 @@ HRESULT CResources::ConvertFBXToMeshBufferData(const wstring _filePath)
 			vert.uv = mesh->HasTextureCoords(0) ? _float2{ mesh->mTextureCoords[0][v].x, mesh->mTextureCoords[0][v].y } : _float2{ 0, 0 };
 
 			vertices.emplace_back(vert);
-		}
-
-		unordered_map<string, _uint> boneNameToIndex;
-		_uint boneIdxCounter = 0;
-
-		for (_uint b = 0; b < mesh->mNumBones; ++b)
-		{
-			aiBone* bone = mesh->mBones[b];
-			string boneName = bone->mName.C_Str();
-
-			_uint boneIdx = 0;
-			auto it = boneNameToIndex.find(boneName);
-			if (it == boneNameToIndex.end())
-			{
-				boneIdx = boneIdxCounter++;
-				boneNameToIndex.insert({ boneName, boneIdx });
-			}
-			else boneIdx = it->second;
-
-			for (_uint w = 0; w < bone->mNumWeights; ++w)
-			{
-				_uint vid = bone->mWeights[w].mVertexId;
-				_float
-					bw = bone->mWeights[w].mWeight;
-				if (vid < vertices.size())
-					CSkinnedMeshBuffer::FillBoneWeights(vertices[vid], boneIdx, bw);
-			}
 		}
 
 		for (_uint f = 0; f < mesh->mNumFaces; ++f)
@@ -199,87 +162,23 @@ HRESULT CResources::ConvertFBXToMeshBufferData(const wstring _filePath)
 			}
 		}
 
-		for (_uint b = 0; b < mesh->mNumBones; ++b)
-		{
-			aiBone* bone = mesh->mBones[b];
-			info.boneNames.push_back(CEngineString::StringToWString(bone->mName.C_Str()));
-
-			const aiMatrix4x4& m = bone->mOffsetMatrix;
-			_float4x4 o = _float4x4(
-				m.a1, m.b1, m.c1, m.d1,
-				m.a2, m.b2, m.c2, m.d2,
-				m.a3, m.b3, m.c3, m.d3,
-				m.a4, m.b4, m.c4, m.d4);
-			info.boneOffsetMatrices.push_back(o);
-		}
-
 		bufferInfoList.push_back(move(info));
 	}
-
-	vector<CSkinnedMeshBuffer::SKINNEDSKELETAL> skeletalHierarchy;
-	unordered_map<aiNode*, _uint> nodeToIdMap;
-
-	// 모든 노드에 대해 transformation/childsId/meshsId/numChild/numMeshes를 채우는 DFS
-	function<void(aiNode*, const _int, vector<CSkinnedMeshBuffer::SKINNEDSKELETAL>&)> TraverseSkeleton =
-		[&](aiNode* node, const _int parentId, vector<CSkinnedMeshBuffer::SKINNEDSKELETAL>& out)
-		{
-			CSkinnedMeshBuffer::SKINNEDSKELETAL n{};
-			n.nodeId = (_int)out.size();
-			n.parentId = parentId;
-			n.name = CEngineString::StringToWString(node->mName.C_Str());
-
-			// 로컬 트랜스폼 복사
-			const aiMatrix4x4& m = node->mTransformation;
-			_float4x4 t = _float4x4
-			(
-				m.a1, m.b1, m.c1, m.d1,
-				m.a2, m.b2, m.c2, m.d2,
-				m.a3, m.b3, m.c3, m.d3,
-				m.a4, m.b4, m.c4, m.d4
-			);
-			n.transformation = t;
-
-			// 이 노드에 연결된 메시 인덱스
-			n.numMeshes = (_uint)node->mNumMeshes;
-			n.meshsId.reserve(node->mNumMeshes);
-			for (_uint mi = 0; mi < node->mNumMeshes; ++mi)
-				n.meshsId.push_back(node->mMeshes[mi]);
-
-			// 우선 푸시한 뒤 자식 재귀
-			out.push_back(n);
-			nodeToIdMap[node] = (_uint)n.nodeId;
-
-			for (_uint ci = 0; ci < node->mNumChildren; ++ci)
-				TraverseSkeleton(node->mChildren[ci], n.nodeId, out);
-
-			// 자식 id 목록/개수 채우기
-			out[n.nodeId].childsId.reserve(node->mNumChildren);
-			for (_uint ci = 0; ci < node->mNumChildren; ++ci)
-			{
-				aiNode* ch = node->mChildren[ci];
-				auto it = nodeToIdMap.find(ch);
-				if (it != nodeToIdMap.end())
-					out[n.nodeId].childsId.push_back((_int)it->second);
-			}
-			out[n.nodeId].numChild = (_uint)out[n.nodeId].childsId.size();
-		};
-
-	TraverseSkeleton(aiScene->mRootNode, -1, skeletalHierarchy);
 
 	auto split = CEngineString::Split(_filePath, L"/");
 	wstring folder = split[split.size() - 2];
 	wstring fileNoExt = CEngineString::Split(split.back(), L".")[0];
 	wstring saveName = folder + L"_" + fileNoExt;
 
-	if (FAILED(SaveSkinnedBufferInfos(
-		L"BinaryAssets/SkinnedMeshData/" + saveName + L".skinneddata",
-		bufferInfoList, skeletalHierarchy)))
+	if (FAILED(SaveMeshBufferInfos(
+		L"BinaryAssets/MeshData/" + saveName + L".meshdata",
+		bufferInfoList)))
 	{
-		CDebug::LogError(L"Failed create skinned mesh Data - can not save: " + _filePath);
+		CDebug::LogError(L"Failed create mesh Data - can not save: " + _filePath);
 		return E_FAIL;
 	}
 
-	CDebug::Log(L"Complete create skinned mesh Data: " + _filePath);
+	CDebug::Log(L"Complete create mesh Data: " + _filePath);
 
 	return S_OK;
 }
