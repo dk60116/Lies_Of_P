2fdab62a8fb5482f15509da0f486a1528cf1a2fb
한택근 <60398466+dk60116@users.noreply.github.com>
2026-02-11 08:42:39 +0900

    Fix skinned bounds function definition placement

--- Project_P/Engine/Header/SkinnedMeshBuffer.cpp
+++ Project_P/Engine/Header/SkinnedMeshBuffer.cpp
@@ -161,6 +161,79 @@ void CSkinnedMeshBuffer::FillBoneWeights(VertexSkinnedBuffer& _targetBuffer, con
         }
     }
 
+
+
+void CSkinnedMeshBuffer::FillBoneWeightsAndIndices(const aiMesh* mesh, vector<VertexSkinnedBuffer>& vertices)
+{
+    //  ε/ġ Ҵ
+    for (_uint i = 0; i < mesh->mNumBones; ++i)
+    {
+        const aiBone* bone = mesh->mBones[i];
+
+        for (_uint j = 0; j < bone->mNumWeights; ++j)
+        {
+            const aiVertexWeight& vw = bone->mWeights[j];
+
+            _uint vertexId = vw.mVertexId;
+            _float weight = vw.mWeight;
+
+            auto& v = vertices[vertexId];
+
+            for (_uint k = 0; k < 4; ++k)
+            {
+                if (v.boneWeights[k] == 0.0f)
+                {
+                    v.boneIndices[k] = i;
+                    v.boneWeights[k] = weight;
+                    break;
+                }
+            }
+        }
+    }
+
+    //  ؽ ġ ū   + ε Բ 
+    for (auto& v : vertices)
+    {
+        // ġ ε  
+        vector<pair<_uint, float>> bonePairs;
+        for (int k = 0; k < 4; ++k)
+        {
+            if (v.boneWeights[k] > 0.0f)
+                bonePairs.emplace_back(v.boneIndices[k], v.boneWeights[k]);
+        }
+
+        // ū ġ  
+        sort(bonePairs.begin(), bonePairs.end(),
+            [](const pair<_uint, float>& a, const pair<_uint, float>& b)
+            {
+                return a.second > b.second;
+            });
+
+        // ٽ 迭 
+        for (_uint k = 0; k < 4; ++k)
+        {
+            if (k < bonePairs.size())
+            {
+                v.boneIndices[k] = bonePairs[k].first;
+                v.boneWeights[k] = bonePairs[k].second;
+            }
+            else
+            {
+                v.boneIndices[k] = 0;
+                v.boneWeights[k] = 0.0f;
+            }
+        }
+
+        // ȭ
+        _float sum = v.boneWeights[0] + v.boneWeights[1] + v.boneWeights[2] + v.boneWeights[3];
+        if (sum > 0.0f)
+        {
+            for (_uint k = 0; k < 4; ++k)
+                v.boneWeights[k] /= sum;
+        }
+    }
+}
+
 _bool CSkinnedMeshBuffer::CalculateDeformedBoundingBox(const vector<CTransform*>& _bones, const _matrix& _meshWorldInv, BoundingBox& _outBox) const
 {
     if (!m_pVertexSysMem || m_sInfo.vertexSize != sizeof(VertexSkinnedBuffer) || m_sInfo.vertextCount == 0)
@@ -245,77 +318,6 @@ _bool CSkinnedMeshBuffer::CalculateDeformedBoundingBox(const vector<CTransform*>
 
     return true;
 }
-
-    // 4개 꽉 찼다면 가장 작은 weight을 대체
-    _uint minIndex = 0;
-    for (int i = 1; i < 4; ++i)
-    {
-        if (_targetBuffer.boneWeights[i] < _targetBuffer.boneWeights[minIndex])
-            minIndex = i;
-    }
-
-    if (_targetBuffer.boneWeights[minIndex] < _weight)
-    {
-        _targetBuffer.boneIndices[minIndex] = _index;
-        _targetBuffer.boneWeights[minIndex] = _weight;
-    }
-}
-
-const _float4x4& CSkinnedMeshBuffer::Get_BoneOffsetMatrix(const _uint _index)
-{
-    return m_vBoneOffsetMatrices[_index];
-}
-
-void CSkinnedMeshBuffer::FillBoneWeightsAndIndices(const aiMesh* mesh, vector<VertexSkinnedBuffer>& vertices)
-{
-    // 본 인덱스/가중치 할당
-    for (_uint i = 0; i < mesh->mNumBones; ++i)
-    {
-        const aiBone* bone = mesh->mBones[i];
-
-        for (_uint j = 0; j < bone->mNumWeights; ++j)
-        {
-            const aiVertexWeight& vw = bone->mWeights[j];
-
-            _uint vertexId = vw.mVertexId;
-            _float weight = vw.mWeight;
-
-            auto& v = vertices[vertexId];
-
-            for (_uint k = 0; k < 4; ++k)
-            {
-                if (v.boneWeights[k] == 0.0f)
-                {
-                    v.boneIndices[k] = i;
-                    v.boneWeights[k] = weight;
-                    break;
-                }
-            }
-        }
-    }
-
-    // 각 버텍스의 가중치를 큰 순서로 정렬 + 인덱스 함께 정렬
-    for (auto& v : vertices)
-    {
-        // 가중치와 인덱스를 쌍으로 모음
-        vector<pair<_uint, float>> bonePairs;
-        for (int k = 0; k < 4; ++k)
-        {
-            if (v.boneWeights[k] > 0.0f)
-                bonePairs.emplace_back(v.boneIndices[k], v.boneWeights[k]);
-        }
-
-        // 큰 가중치 순으로 정렬
-        sort(bonePairs.begin(), bonePairs.end(),
-            [](const pair<_uint, float>& a, const pair<_uint, float>& b)
-            {
-                return a.second > b.second;
-            });
-
-        // 다시 배열에 복사
-        for (_uint k = 0; k < 4; ++k)
-        {
-            if (k < bonePairs.size())
             {
                 v.boneIndices[k] = bonePairs[k].first;
                 v.boneWeights[k] = bonePairs[k].second;
