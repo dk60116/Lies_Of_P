6e291f4fcf067133f26edb54272cbd2061c42c4b
HTK <dk60116@gmail.com>
2026-02-19 14:26:41 +0900

    Feat:

---
+++ Project_P/Reference/Header/CapsuleCollider.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Collider.h"
+
+NS_BEGIN(Engine)
+
+class ENGINE_DLL CCapsuleCollider : public CCollider
+{
+protected:
+    explicit CCapsuleCollider();
+    ~CCapsuleCollider();
+
+public:
+    static CCapsuleCollider* Create();
+    CComponent* Clone() const override;
+
+public:
+    HRESULT Initialize() override;
+    void Update() override;
+    void FixedUpdate() override;
+    void Render_Editor() override;
+    void Render_Gizmo() override;
+
+    const _float GetRadius() const;
+    const _float GetHeight() const;
+    void SetRadius(const _float radius);
+    void SetHeight(const _float height);
+
+    void OnDestroy() override;
+
+protected:
+    void BuildShapeIfNeeded() override;
+
+private:
+    _float m_fRadius;
+    _float m_fHeight;
+    class CMeshBuffer* m_pLineMesh;
+    class CMaterial* m_pLineMaterial;
+};
+
+NS_END
