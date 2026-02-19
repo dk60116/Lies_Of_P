625a2711cb2ce9b8b9ba10b0f7c4d25067ef5cd9
한택근 <60398466+dk60116@users.noreply.github.com>
2026-02-20 07:30:41 +0900

    Use ImGuizmo state for rigidbody transform override

--- Project_P/Engine/Code/RigidBody.cpp
+++ Project_P/Engine/Code/RigidBody.cpp
@@ -3,6 +3,7 @@
 #include "Collider.h"
 #ifndef _CLIENT_BUILD
 #include "Editor.h"
+#include "ImGuizmo.h"
 #endif
 
 #include <algorithm>
@@ -525,22 +526,7 @@ void CRigidBody::SyncDynamicFromJolt()
 #ifndef _CLIENT_BUILD
     CGameObject* selected = CEditor::GetInstance().Get_SelectedGameObject();
     if (selected != nullptr && selected == m_pGameObject)
-    {
-        const vector3& prevPos = Get_Transform()->Get_PrevPosition();
-        const quaternion& prevRot = Get_Transform()->Get_PrevQuaternion();
-        const vector3& currPos = Get_Transform()->Get_Position();
-        const quaternion currRot = Get_Transform()->Get_Quaternion();
-
-        const float dx = currPos.x - prevPos.x;
-        const float dy = currPos.y - prevPos.y;
-        const float dz = currPos.z - prevPos.z;
-        const float posDiffSq = dx * dx + dy * dy + dz * dz;
-
-        const float rotDot = prevRot.x * currRot.x + prevRot.y * currRot.y + prevRot.z * currRot.z + prevRot.w * currRot.w;
-        const float absRotDot = fabsf(rotDot);
-
-        forceTransformOverride = posDiffSq > 0.000001f || absRotDot < 0.9999f;
-    }
+        forceTransformOverride = ImGuizmo::IsUsing();
 #endif
 
     const RVec3 joltPos = GetBI().GetPosition(sourceBodyId);
