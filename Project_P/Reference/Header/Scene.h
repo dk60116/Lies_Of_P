bdce640af7cf2395f818c7a827f654c81d0c4b94
HTK <dk60116@gmail.com>
2026-02-20 15:31:26 +0900

    Feat: Physics

--- Project_P/Reference/Header/Scene.h
+++ Project_P/Reference/Header/Scene.h
@@ -37,6 +37,15 @@ public:
         _float4 localQuaternion = {};
         _float3 localScale = {};
         _bool isActive = true;
+        _bool rigidBodyKinematic = false;
+        _bool rigidBodyUseGravity = true;
+        _float rigidBodyMass = 1.f;
+        _bool rigidBodyConstPositionX = false;
+        _bool rigidBodyConstPositionY = false;
+        _bool rigidBodyConstPositionZ = false;
+        _bool rigidBodyConstRotationX = false;
+        _bool rigidBodyConstRotationY = false;
+        _bool rigidBodyConstRotationZ = false;
         _bool isRect = false;
         SCENERECTINFO rectInfo = {};
         vector<wstring> componentNames = {};
