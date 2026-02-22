d06a2c798ee8666ba82f115ddd081352a81d8193
HTK <dk60116@gmail.com>
2026-02-23 04:19:05 +0900

    Feat: Evade

--- Project_P/Client/Code/PlayerState_Evade.cpp
+++ Project_P/Client/Code/PlayerState_Evade.cpp
@@ -136,7 +136,7 @@ void CPlayerState_Evade::Update()
 	__super::Update();
 
 	if (m_bIsDash)
-		m_pCtx->AddPosition(m_pCtx->PlayerForward() * DELTA_TIME * 9.f * (m_bInputDir ? 1.f : -1.f));
+		m_pCtx->AddPosition(m_pCtx->PlayerForward() * DELTA_TIME * m_pCtx->PlayerStatus().evadeLength * (m_bInputDir ? 1.f : -1.f));
 
 	if (m_pCtx->IsKeyPressed_Down(CPlayerController::PlayerState::Attack))
 		m_bAttackBuffer = true;
--- Project_P/Client/Header/Player.h
+++ Project_P/Client/Header/Player.h
@@ -26,6 +26,7 @@ public:
 		_float bigTurnStopSec = 0.28f;
 		_float focusTurnRatio = 8.f;
 		_int attackPower = 1;
+		_float evadeLength = 3.5f;
 		_float jumpPower = 300.f;
 	};
 
