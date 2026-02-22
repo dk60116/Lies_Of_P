067eea75b3491f9d71b781b2f33b3f85c85aa742
한택근 <60398466+dk60116@users.noreply.github.com>
2026-02-22 21:38:33 +0900

    Increase rigidbody move speed and remove contact slowdown

--- Project_P/Engine/Code/RigidBody.cpp
+++ Project_P/Engine/Code/RigidBody.cpp
@@ -527,12 +527,7 @@ void CRigidBody::Translate(const vector3& _deltaWorld)
         {
             const Vec3 currentLinearVelocity = GetBI().GetLinearVelocity(m_iBodyID);
             const float resolvedY = fabsf(appliedDelta.y) > 1e-6f ? targetVelocity.GetY() : currentLinearVelocity.GetY();
-            _float horizontalSpeedScale = 1.8f;
-            if (CCollider* bodyCollider = GetEventCollider(false))
-            {
-                if (bodyCollider->IsContacting())
-                    horizontalSpeedScale = 1.0f;
-            }
+            const _float horizontalSpeedScale = 2.4f;
             const Vec3 resolvedLinearVelocity(targetVelocity.GetX() * horizontalSpeedScale, resolvedY, targetVelocity.GetZ() * horizontalSpeedScale);
 
             GetBI().SetLinearAndAngularVelocity(m_iBodyID, resolvedLinearVelocity, GetBI().GetAngularVelocity(m_iBodyID));
@@ -558,12 +553,7 @@ void CRigidBody::Translate(const vector3& _deltaWorld)
         {
             const Vec3 currentLinearVelocity = GetBI().GetLinearVelocity(m_iSensorBodyID);
             const float resolvedY = fabsf(appliedDelta.y) > 1e-6f ? targetVelocity.GetY() : currentLinearVelocity.GetY();
-            _float horizontalSpeedScale = 1.8f;
-            if (CCollider* sensorCollider = GetEventCollider(false))
-            {
-                if (sensorCollider->IsContacting())
-                    horizontalSpeedScale = 1.0f;
-            }
+            const _float horizontalSpeedScale = 2.4f;
             const Vec3 resolvedLinearVelocity(targetVelocity.GetX() * horizontalSpeedScale, resolvedY, targetVelocity.GetZ() * horizontalSpeedScale);
 
             GetBI().SetLinearAndAngularVelocity(m_iSensorBodyID, resolvedLinearVelocity, GetBI().GetAngularVelocity(m_iSensorBodyID));
