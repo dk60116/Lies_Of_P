0d6f62be5028150f4af7334e24b31fb249d64039
한택근 <60398466+dk60116@users.noreply.github.com>
2026-02-27 01:53:23 +0900

    Add skinned mesh inspector import and material UI

--- Project_P/Engine/Code/InspectorBox.cpp
+++ Project_P/Engine/Code/InspectorBox.cpp
@@ -79,6 +79,17 @@ struct PendingMeshSelectionRequest
 
 static PendingMeshSelectionRequest g_pendingMeshSelectionRequest;
 
+struct PendingSkinnedMeshSelectionRequest
+{
+    CGameObject* obj = nullptr;
+    CSkinnedMeshRenderer* skinnedMeshRenderer = nullptr;
+    string relPath;
+    _bool selectedSkinnedData = false;
+    _bool pending = false;
+};
+
+static PendingSkinnedMeshSelectionRequest g_pendingSkinnedMeshSelectionRequest;
+
 static vector<string> SplitBySlash(const string& input)
 {
     vector<string> parts;
@@ -453,7 +464,7 @@ static string MakeUniqueSceneEntryName(const string& base)
     return base + "_Auto_" + to_string(ms);
 }
 
-static _bool EnsureSceneMeshEntry(const fs::path& scenePath, const string& assetRelPath, string& outEntryName)
+static _bool EnsureSceneEntryWithFormat(const fs::path& scenePath, const string& assetRelPath, const string& format, string& outEntryName)
 {
     ifstream in(scenePath);
     if (!in.is_open())
@@ -464,7 +475,6 @@ static _bool EnsureSceneMeshEntry(const fs::path& scenePath, const string& asset
 
     while (getline(in, line))
     {
-
         if (line.empty() || CEngineString::Contains(line, "//") || !CEngineString::Contains(line, " : "))
             continue;
 
@@ -488,8 +498,18 @@ static _bool EnsureSceneMeshEntry(const fs::path& scenePath, const string& asset
     if (!out.is_open())
         return false;
 
-    out << outEntryName << " : " << normalizedTarget << " : [Mesh]" << "\n";
+    out << outEntryName << " : " << normalizedTarget << " : " << format << "\n";
     return true;
+}
+
+static _bool EnsureSceneMeshEntry(const fs::path& scenePath, const string& assetRelPath, string& outEntryName)
+{
+    return EnsureSceneEntryWithFormat(scenePath, assetRelPath, "[Mesh]", outEntryName);
+}
+
+static _bool EnsureSceneSkinnedMeshEntry(const fs::path& scenePath, const string& assetRelPath, string& outEntryName)
+{
+    return EnsureSceneEntryWithFormat(scenePath, assetRelPath, "[Skinned Mesh]", outEntryName);
 }
 
 static _bool RemoveFirstMeshRendererComponent(CGameObject* obj)
@@ -546,6 +566,19 @@ static vector<MeshBundle> EnsureSceneMeshBundles(const wstring& sceneMeshResourc
 
     CResources::GetInstance().CreateSceneMeshBundle(sceneMeshResourceName, meshInfos, FILTER_MESHBUFFER | FILTER_MATERIAL, nullptr, false);
     return CResources::GetInstance().LoadMeshBuffersOnScene(sceneMeshResourceName);
+}
+
+static vector<SkinnedMeshBundle> EnsureSceneSkinnedMeshBundles(const wstring& sceneMeshResourceName, const string& skinnedDataFile)
+{
+    vector<SkinnedMeshBundle> bundles = CResources::GetInstance().LoadSkinnedMeshBuffersOnScene(sceneMeshResourceName);
+    if (!bundles.empty())
+        return bundles;
+
+    auto skinnedInfos = CResources::GetInstance().ReadSkinnedBufferInfos(CEngineString::StringToWString(skinnedDataFile));
+    if (skinnedInfos.initList.empty())
+        return {};
+
+    return CResources::GetInstance().CreateSceneSkinnedBundle(sceneMeshResourceName, skinnedInfos.initList, skinnedInfos.skeletalList, FILTER_MESHBUFFER | FILTER_MATERIAL, nullptr, false);
 }
 
 static void ApplyMeshSelectionToObject(CGameObject* obj, CMeshFilter* meshFilter, const string& relPath, const _bool selectedMeshData)
@@ -642,6 +675,140 @@ static void ProcessPendingMeshSelectionRequest()
         return;
 
     ApplyMeshSelectionToObject(req.obj, req.meshFilter, req.relPath, req.selectedMeshData);
+}
+
+static void ApplySkinnedMeshSelectionToObject(CGameObject* obj, CSkinnedMeshRenderer* skinnedMeshRenderer, const string& relPath, const _bool selectedSkinnedData)
+{
+    if (!obj || !skinnedMeshRenderer)
+        return;
+
+    CScene* scene = CSceneManager::GetInstance().Get_CrtScene();
+    if (!scene)
+        return;
+
+    const string normalizedRelPath = NormalizeSlashPath(relPath);
+    string sceneEntryName = selectedSkinnedData ? fs::path(normalizedRelPath).stem().string() : string();
+
+    if (!selectedSkinnedData)
+    {
+        const string ext = CEditor::ToLowerCopy(fs::path(normalizedRelPath).extension().string());
+        const fs::path assetPath = fs::path("../Assets") / fs::path(normalizedRelPath);
+        if (ext != ".fbx" || !fs::exists(assetPath))
+            return;
+
+        const fs::path scenePath = fs::path("../Assets/Scenes") / (CEngineString::WStringToString(scene->Get_SceneName()) + ".scene");
+        if (!EnsureSceneSkinnedMeshEntry(scenePath, normalizedRelPath, sceneEntryName))
+            return;
+    }
+
+    const string expectedSkinnedDataFile = sceneEntryName + ".skinneddata";
+    const fs::path expectedSkinnedDataPath = fs::path("BinaryAssets/SkinnedMeshData") / expectedSkinnedDataFile;
+    const _bool skinnedDataExistsInitially = fs::exists(expectedSkinnedDataPath);
+    const wstring sceneMeshResourceName = CEngineString::StringToWString(sceneEntryName + " (MeshBuffer)");
+
+    if (skinnedDataExistsInitially)
+    {
+        vector<SkinnedMeshBundle> bundles = EnsureSceneSkinnedMeshBundles(sceneMeshResourceName, expectedSkinnedDataFile);
+        if (bundles.empty() || !bundles[0].meshBuffer)
+            return;
+
+        skinnedMeshRenderer->Set_MeshBuffer(bundles[0].meshBuffer);
+        if (bundles[0].material)
+            skinnedMeshRenderer->Set_Material(bundles[0].material);
+        return;
+    }
+
+    if (selectedSkinnedData)
+        return;
+
+    CResources::GetInstance().ConvertFBXToSkinnedBufferData(CEngineString::StringToWString(normalizedRelPath));
+
+    const string convertedBase = BuildMeshDataBaseName(normalizedRelPath);
+    const fs::path convertedSkinnedDataPath = fs::path("BinaryAssets/SkinnedMeshData") / (convertedBase + ".skinneddata");
+    if (!fs::exists(convertedSkinnedDataPath))
+        return;
+
+    if (convertedSkinnedDataPath != expectedSkinnedDataPath)
+    {
+        error_code ec;
+        fs::copy_file(convertedSkinnedDataPath, expectedSkinnedDataPath, fs::copy_options::overwrite_existing, ec);
+        if (ec)
+            return;
+    }
+
+    vector<SkinnedMeshBundle> bundles = EnsureSceneSkinnedMeshBundles(sceneMeshResourceName, expectedSkinnedDataFile);
+    if (bundles.empty() || !bundles[0].meshBuffer)
+        return;
+
+    skinnedMeshRenderer->Set_MeshBuffer(bundles[0].meshBuffer);
+    if (bundles[0].material)
+        skinnedMeshRenderer->Set_Material(bundles[0].material);
+}
+
+static void QueueSkinnedMeshSelectionRequest(CGameObject* obj, CSkinnedMeshRenderer* skinnedMeshRenderer, const string& relPath, const _bool selectedSkinnedData)
+{
+    if (!obj || !skinnedMeshRenderer)
+        return;
+
+    g_pendingSkinnedMeshSelectionRequest.obj = obj;
+    g_pendingSkinnedMeshSelectionRequest.skinnedMeshRenderer = skinnedMeshRenderer;
+    g_pendingSkinnedMeshSelectionRequest.relPath = relPath;
+    g_pendingSkinnedMeshSelectionRequest.selectedSkinnedData = selectedSkinnedData;
+    g_pendingSkinnedMeshSelectionRequest.pending = true;
+}
+
+static void ProcessPendingSkinnedMeshSelectionRequest()
+{
+    if (!g_pendingSkinnedMeshSelectionRequest.pending)
+        return;
+
+    PendingSkinnedMeshSelectionRequest req = g_pendingSkinnedMeshSelectionRequest;
+    g_pendingSkinnedMeshSelectionRequest = {};
+
+    if (!req.obj || !req.skinnedMeshRenderer)
+        return;
+
+    ApplySkinnedMeshSelectionToObject(req.obj, req.skinnedMeshRenderer, req.relPath, req.selectedSkinnedData);
+}
+
+static void RenderSkinnedPathTreeRecursive(const PathTreeNode& node, const string& idPrefix, CGameObject* obj, CSkinnedMeshRenderer* skinnedMeshRenderer, const _bool selectedSkinnedData)
+{
+    for (const auto& childPair : node.children)
+    {
+        const string& name = childPair.first;
+        const PathTreeNode& child = childPair.second;
+
+        if (child.children.empty() && child.isFile)
+        {
+            const string label = name + "##" + idPrefix + child.fullPath;
+            if (ImGui::Selectable(label.c_str(), false))
+                QueueSkinnedMeshSelectionRequest(obj, skinnedMeshRenderer, child.fullPath, selectedSkinnedData);
+            continue;
+        }
+
+        const string nodeKey = child.fullPath.empty() ? name : child.fullPath;
+        const string label = name + "##" + idPrefix + nodeKey;
+        if (ImGui::TreeNode(label.c_str()))
+        {
+            RenderSkinnedPathTreeRecursive(child, idPrefix, obj, skinnedMeshRenderer, selectedSkinnedData);
+            ImGui::TreePop();
+        }
+    }
+}
+
+static void RenderSkinnedPathTreeList(const vector<string>& files, const string& idPrefix, CGameObject* obj, CSkinnedMeshRenderer* skinnedMeshRenderer, const _bool selectedSkinnedData, const string& emptyText)
+{
+    if (files.empty())
+    {
+        ImGui::Selectable(emptyText.c_str(), false, ImGuiSelectableFlags_Disabled);
+        return;
+    }
+
+    PathTreeNode root;
+    for (const string& relPath : files)
+        AddPathToTree(root, relPath);
+
+    RenderSkinnedPathTreeRecursive(root, idPrefix, obj, skinnedMeshRenderer, selectedSkinnedData);
 }
 
 static void RenderPathTreeRecursive(const PathTreeNode& node, const string& idPrefix, CGameObject* obj, CMeshFilter* meshFilter, const _bool selectedMeshData)
@@ -969,6 +1136,7 @@ void CInspectorBox::Render()
 
         ShowComponents(selectedObj);
         ProcessPendingMeshSelectionRequest();
+        ProcessPendingSkinnedMeshSelectionRequest();
     }
     else
         ImGui::Text("No object selected.");
@@ -1382,6 +1550,9 @@ void CInspectorBox::ShowComponents(CGameObject* _obj)
             if (CMeshRenderer* meshRenderer = dynamic_cast<CMeshRenderer*>(component))
                 RenderMeshRendererComponent(meshRenderer);
 
+            if (CSkinnedMeshRenderer* skinnedMeshRenderer = dynamic_cast<CSkinnedMeshRenderer*>(component))
+                RenderSkinnedMeshRendererComponent(_obj, skinnedMeshRenderer);
+
             if (CMeshFilter* meshFilter = dynamic_cast<CMeshFilter*>(component))
                 RenderMeshFilterComponent(_obj, meshFilter);
 
@@ -1710,6 +1881,234 @@ void CInspectorBox::RenderMeshRendererComponent(CMeshRenderer* _meshRenderer)
 
     string meshName = CEngineString::WStringToString(meshBuffer->Get_ResourceName());
     ImGui::Text("MeshBuffer: %s", meshName.c_str());
+}
+
+void CInspectorBox::RenderSkinnedMeshRendererComponent(CGameObject* _obj, CSkinnedMeshRenderer* _skinnedMeshRenderer)
+{
+    if (!_obj || !_skinnedMeshRenderer)
+        return;
+
+    CMaterial* material = _skinnedMeshRenderer->Get_Material();
+    if (material)
+    {
+        string materialName = CEngineString::WStringToString(material->Get_ResourceName());
+        ImGui::Text("Material: %s", materialName.c_str());
+
+        CShader* shader = material->Get_Shader();
+        if (shader)
+        {
+            string shaderName = CEngineString::WStringToString(shader->Get_ResourceName());
+            ImGui::Text("Shader: %s", shaderName.c_str());
+        }
+        else
+        {
+            ImGui::TextUnformatted("Shader: None");
+        }
+
+        if (ImGui::Button("Add"))
+            material->Set_Texture(nullptr, static_cast<_int>(material->Get_TextureCount()));
+
+        ImGui::TextUnformatted("Textures:");
+        const _uint textureCount = material->Get_TextureCount();
+        if (textureCount == 0)
+        {
+            ImGui::BulletText("None");
+        }
+        else
+        {
+            for (_uint i = 0; i < textureCount; ++i)
+            {
+                CTexture* texture = material->Get_Texture(static_cast<_int>(i));
+                string textureName = texture ? CEngineString::WStringToString(texture->Get_ResourceName()) : "None";
+
+                ImGui::PushID(static_cast<int>(i));
+                _bool openPicker = false;
+
+                if (texture && texture->Get_SRV())
+                    openPicker = ImGui::ImageButton("##TextureThumb", ImTextureRef((ImTextureID)(intptr_t)texture->Get_SRV()), ImVec2(20.f, 20.f));
+                else
+                    openPicker = ImGui::Button("Select", ImVec2(52.f, 20.f));
+
+                if (ImGui::IsItemHovered())
+                    ImGui::SetTooltip("%s", textureName.c_str());
+
+                if (openPicker)
+                    OpenTexturePicker(material, static_cast<_int>(i));
+
+                ImGui::SameLine();
+                const string shortSlotName = BuildShortLabel(textureName, 18);
+                ImGui::Text("[%u] %s", i, shortSlotName.c_str());
+                if (ImGui::IsItemHovered())
+                    ImGui::SetTooltip("%s", textureName.c_str());
+
+                if (ImGui::BeginPopupContextItem("TextureContext"))
+                {
+                    if (ImGui::MenuItem("Clear"))
+                        material->Set_Texture(nullptr, static_cast<_int>(i));
+                    if (ImGui::MenuItem("Remove Slot"))
+                        material->RemoveTextureSlot(static_cast<_int>(i));
+                    ImGui::EndPopup();
+                }
+
+                ImGui::PopID();
+            }
+        }
+
+        if (ImGui::TreeNode("Material Properties"))
+        {
+            for (const auto& [key, value] : material->Get_FloatValues())
+            {
+                _float v = value;
+                const string keyStr = CEngineString::WStringToString(key);
+                if (ImGui::InputFloat((keyStr + "##CustomFloat").c_str(), &v))
+                    material->Set_FloatValue(key, v);
+            }
+
+            for (const auto& [key, value] : material->Get_IntValues())
+            {
+                _int v = value;
+                const string keyStr = CEngineString::WStringToString(key);
+                if (ImGui::InputInt((keyStr + "##CustomInt").c_str(), &v))
+                    material->Set_IntValue(key, v);
+            }
+
+            for (const auto& [key, value] : material->Get_Vector2Values())
+            {
+                _float2 v = value;
+                _float arr[2] = { v.x, v.y };
+                const string keyStr = CEngineString::WStringToString(key);
+                if (ImGui::InputFloat2((keyStr + "##CustomVec2").c_str(), arr))
+                    material->Set_Vector2Value(key, { arr[0], arr[1] });
+            }
+
+            for (const auto& [key, value] : material->Get_Vector3Values())
+            {
+                _float3 v = value;
+                _float arr[3] = { v.x, v.y, v.z };
+                const string keyStr = CEngineString::WStringToString(key);
+                if (ImGui::InputFloat3((keyStr + "##CustomVec3").c_str(), arr))
+                    material->Set_Vector3Value(key, { arr[0], arr[1], arr[2] });
+            }
+
+            for (const auto& [key, value] : material->Get_Vector4Values())
+            {
+                _float4 v = value;
+                _float arr[4] = { v.x, v.y, v.z, v.w };
+                const string keyStr = CEngineString::WStringToString(key);
+                if (ImGui::InputFloat4((keyStr + "##CustomVec4").c_str(), arr))
+                    material->Set_Vector4Value(key, { arr[0], arr[1], arr[2], arr[3] });
+            }
+
+            for (const auto& [key, value] : material->Get_MatrixValues())
+            {
+                const string keyStr = CEngineString::WStringToString(key);
+                if (ImGui::TreeNode((keyStr + "##CustomMat").c_str()))
+                {
+                    _float4x4 mat = value;
+                    _float row1[4] = { mat._11, mat._12, mat._13, mat._14 };
+                    _float row2[4] = { mat._21, mat._22, mat._23, mat._24 };
+                    _float row3[4] = { mat._31, mat._32, mat._33, mat._34 };
+                    _float row4[4] = { mat._41, mat._42, mat._43, mat._44 };
+
+                    _bool changed = false;
+                    changed |= ImGui::InputFloat4(("R1##" + keyStr).c_str(), row1);
+                    changed |= ImGui::InputFloat4(("R2##" + keyStr).c_str(), row2);
+                    changed |= ImGui::InputFloat4(("R3##" + keyStr).c_str(), row3);
+                    changed |= ImGui::InputFloat4(("R4##" + keyStr).c_str(), row4);
+
+                    if (changed)
+                    {
+                        _float4x4 newMat = {};
+                        newMat._11 = row1[0]; newMat._12 = row1[1]; newMat._13 = row1[2]; newMat._14 = row1[3];
+                        newMat._21 = row2[0]; newMat._22 = row2[1]; newMat._23 = row2[2]; newMat._24 = row2[3];
+                        newMat._31 = row3[0]; newMat._32 = row3[1]; newMat._33 = row3[2]; newMat._34 = row3[3];
+                        newMat._41 = row4[0]; newMat._42 = row4[1]; newMat._43 = row4[2]; newMat._44 = row4[3];
+                        material->Set_MatrixValue(key, newMat);
+                    }
+
+                    ImGui::TreePop();
+                }
+            }
+
+            ImGui::TreePop();
+        }
+    }
+    else
+    {
+        ImGui::TextUnformatted("Material: None");
+        ImGui::TextUnformatted("Shader: None");
+        ImGui::TextUnformatted("Textures: None");
+    }
+
+    vector<pair<string, CSkinnedMeshBuffer*>> meshOptions;
+    vector<string> fbxFiles = CollectRelativeFilesByExtension(fs::path(L"../Assets"), ".fbx");
+    vector<string> skinnedDataFiles = CollectRelativeFilesByExtension(fs::path(L"BinaryAssets/SkinnedMeshData"), ".skinneddata");
+    CResources& resources = CResources::GetInstance();
+
+    for (auto& entry : resources.m_mGameResourceList)
+    {
+        CSkinnedMeshBuffer* meshBuffer = dynamic_cast<CSkinnedMeshBuffer*>(entry.second);
+        if (!meshBuffer)
+            continue;
+
+        string resourceName = CEngineString::WStringToString(meshBuffer->Get_ResourceName());
+        meshOptions.push_back({ resourceName, meshBuffer });
+    }
+
+    sort(meshOptions.begin(), meshOptions.end(), [](const auto& a, const auto& b)
+    {
+        return a.first < b.first;
+    });
+
+    CSkinnedMeshBuffer* currentMeshBuffer = _skinnedMeshRenderer->Get_SkinnedMeshBuffer();
+    string currentName = "None";
+    _int currentIndex = -1;
+
+    for (_uint i = 0; i < meshOptions.size(); ++i)
+    {
+        if (meshOptions[i].second == currentMeshBuffer)
+        {
+            currentIndex = static_cast<_int>(i);
+            currentName = meshOptions[i].first;
+            break;
+        }
+    }
+
+    const string comboLabel = "SkinnedMeshBuffer##" + to_string(_obj->Get_UniqueID()) + "_" + to_string(reinterpret_cast<uintptr_t>(_skinnedMeshRenderer));
+
+    if (ImGui::BeginCombo(comboLabel.c_str(), currentName.c_str()))
+    {
+        if (ImGui::Selectable("None", currentMeshBuffer == nullptr))
+            _skinnedMeshRenderer->Set_MeshBuffer(nullptr);
+
+        for (_uint i = 0; i < meshOptions.size(); ++i)
+        {
+            const bool selected = (static_cast<_int>(i) == currentIndex);
+            if (ImGui::Selectable(meshOptions[i].first.c_str(), selected))
+                _skinnedMeshRenderer->Set_MeshBuffer(meshOptions[i].second);
+
+            if (selected)
+                ImGui::SetItemDefaultFocus();
+        }
+
+        ImGui::EndCombo();
+    }
+
+    if (ImGui::TreeNode("Assets .fbx (Skinned)"))
+    {
+        if (ImGui::BeginChild("##skinned_fbx_tree_box", ImVec2(0.f, 180.f), true))
+            RenderSkinnedPathTreeList(fbxFiles, "skinned_fbx_tree_", _obj, _skinnedMeshRenderer, false, "(No .fbx files)");
+        ImGui::EndChild();
+        ImGui::TreePop();
+    }
+
+    if (ImGui::TreeNode("BinaryAssets .skinneddata"))
+    {
+        if (ImGui::BeginChild("##skinneddata_tree_box", ImVec2(0.f, 180.f), true))
+            RenderSkinnedPathTreeList(skinnedDataFiles, "skinneddata_tree_", _obj, _skinnedMeshRenderer, true, "(No .skinneddata files)");
+        ImGui::EndChild();
+        ImGui::TreePop();
+    }
 }
 
 void CInspectorBox::RenderMeshFilterComponent(CGameObject* _obj, CMeshFilter* _meshFilter)
